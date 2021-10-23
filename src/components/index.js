eb1744c7e66200a18e84faa93140f2bf9ede3ce5
chapuzzo <luismax@gmail.com>
2021-10-23 15:42:01 +0200

    reworked exporting

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,5 @@
-import Vue from "vue"
 import List from "./List"
 
-const Components = {
+export {
   List
 }
-
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name])
-})
-
-export default Components
